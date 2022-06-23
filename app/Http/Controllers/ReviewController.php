a9bf26a8772e5ba23fbe2aa451fda40e74137505
PhanPhanit <phanphanit.pp12345@gmail.com>
2022-06-23 11:51:25 +0700

    fix

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -58,8 +58,8 @@ class ReviewController extends Controller
         $updateProduct = DB::table('reviews')
             ->select(DB::raw('count(*) as numOfReviews, round(avg(rating),1) as averageRating'))
             ->where('product', '=', $request->input('product'))
-            ->first()->toArray();
-        return $updateProduct['numOfReviews'];
+            ->first();
+        return $updateProduct->get('numOfReviews');
         // $product->numOfReviews = $updateProduct->numOfReviews;
         // $product->averageRating = $updateProduct->averageRating;
         // $product->save();
